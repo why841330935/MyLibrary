6c6f455cc25dd250be4b77f32fdcf2fd8bd03da1
v_wanghuanyu <wang.huanyu@neusoft.com>
2016-12-09 17:31:37 +0800

    2016/12/09  通用banner添加，demo事例添加

---
+++ app/src/main/java/com/example/admin/myapplication/BaseApplication.java
@@ -0,0 +1,14 @@
+package com.example.admin.myapplication;
+
+import com.why.library.WBaseApplication;
+
+/**
+ * Created by admin on 2016/11/30.
+ */
+
+public class BaseApplication extends WBaseApplication{
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
--- app/src/main/java/com/example/admin/myapplication/MainActivity.java
+++ app/src/main/java/com/example/admin/myapplication/MainActivity.java
@@ -1,24 +1,32 @@
 package com.example.admin.myapplication;
 
 import android.content.Intent;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.admin.myapplication.module.TestModule;
 import com.example.admin.myapplication.presenter.TestPresenter;
+import com.why.library.common.banner.Banner;
 import com.why.library.rxandroid.annotation.AcceptElement;
 import com.why.library.tool.WLogTool;
 import com.why.library.ui.WApiCompatActivity;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 import butterknife.Bind;
 
 public class MainActivity extends WApiCompatActivity<TestPresenter,TestModule> {
     @Bind(R.id.text) TextView text;
     @Bind(R.id.bt) Button bt;
+    @Bind(R.id.banner2) Banner banner2;
 
     @Override
     protected int getLayoutResource() {
@@ -28,6 +36,29 @@ public class MainActivity extends WApiCompatActivity<TestPresenter,TestModule> {
     @Override
     protected void initTodo(Bundle savedInstanceState) {
 //        presenter.test();
+        String[] urls = getResources().getStringArray(R.array.url4);
+        List list = Arrays.asList(urls);
+        List<?> images = new ArrayList(list);
+        banner2.setImages(images);
+        banner2.isAutoPlay(false);
+        banner2.setOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                Toast.makeText(MainActivity.this,"点击" + position,Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
+        banner2.start();
+
     }
 
     @Override
